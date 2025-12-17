208c187081b587be395909247c774ee310ef37c8
JRoLee <cnsfw123@gmail.com>
2025-12-17 21:03:26 +0900

    First commit
    First commit

---
+++ Third_Task_Inventory/Inventory/Inventory.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Inventory.h"
+
+template <class T>
+Inventory<T>::Inventory(int cap) : capacity(cap), size(0),
+{
+	pItems[] = pItems[cap];
+}
+
---
+++ Third_Task_Inventory/Inventory/Inventory.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+template <typename T>
+class Inventory
+{
+public:
+	Inventory(int capacity = 10);
+	~Inventory();
+	void AddItem(const T& item);
+	void RemoveLastItem();
+	void printAllItems() const;
+
+	//getter
+	int GstSize() const;
+	int GetCapacity() const;
+
+	private:
+	T* pItems[];
+	int capacity;
+	int size;
+};
+
